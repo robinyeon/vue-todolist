399edd9bb995a7c2090492627142ce17e4f4b86a
robinyeon <robin.yeon@gmail.com>
2023-03-01 22:39:33 +0900

    Install axios

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,20 +2,12 @@
   <div>
     <form>
       <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Email</label>
-        <input
-          type="password"
-          class="form-control"
-          id="exampleInputPassword1"
-        />
+        <label for="inputEmail" class="form-label">Email</label>
+        <input type="email" id="inputEmail" class="form-control" />
       </div>
       <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input
-          type="password"
-          class="form-control"
-          id="exampleInputPassword1"
-        />
+        <label for="inputPassword" class="form-label">Password</label>
+        <input type="password" class="form-control" id="inputPassword" />
       </div>
 
       <button type="submit" class="btn btn-primary">Login</button>
